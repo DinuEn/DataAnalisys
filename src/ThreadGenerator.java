3755a1c19aadda3a2e6640db739bfba763f77c05
Enescu Dinu <dinu.enescu@gmail.com>
2021-05-30 15:05:57 +0300

    Added ThreadGenerator

--- src/ThreadGenerator.java
+++ src/ThreadGenerator.java
@@ -1,3 +1,15 @@
-public class ThreadGenerator {
+import java.util.ArrayList;
 
+public interface ThreadGenerator {
+
+    static ArrayList<Thread> generateThreads(int nOfThreads){
+        Runnable myRunnable = new DataUtilThread();
+        ArrayList<Thread> threadArrayList = new ArrayList<>();
+
+        for(int i = 0; i < nOfThreads; i++){
+            threadArrayList.add(new Thread(myRunnable));
+        }
+
+        return threadArrayList;
+    }
 }
