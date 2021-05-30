797f6534d2e8a22a3d812261f52d35c8937dd1a6
tomover9000 <vlad.zorlescu@gmail.com>
2021-05-30 16:13:54 +0300

    GIANOURMOUS COMMIT

--- src/ThreadManager.java
+++ src/ThreadManager.java
@@ -1,37 +1,50 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class ThreadManager {
     private ArrayList<Thread> threadArrayList = null;
     private int nOfThreads = 0;
     private ArrayList<String> dataList= null;
-    private int opCode = 0;
     private int functionCode = 0;
+    private ArrayList<DataUtilThread> runnableList;
 
 
-    public ThreadManager(int nOfThreads, ArrayList<String> dataList, int opCode, int functionCode) {
+    public ThreadManager(int nOfThreads, ArrayList<String> dataList, int functionCode) {
         this.nOfThreads = nOfThreads;
         this.dataList = dataList;
-        this.opCode = opCode;
         this.functionCode = functionCode;
+        runnableList = new ArrayList<>();
     }
 
-
     public void setThreadArrayList(ArrayList<Thread> threadArrayList) {
-            ArrayList<DataUtilThread> runnableList= new ArrayList<>();
-
+            int subListLen = (int) dataList.size() / nOfThreads;
             for(int i = 0; i < nOfThreads; i++){
-                ArrayList<String> subList = (ArrayList<String>) dataList.subList(dataList.size()*i,dataList.size()*(i+1)) ;
-                runnableList.add(new DataUtilThread(subList, opCode ));
+                List<String> subList = dataList.subList(subListLen * i, subListLen * (i + 1));
+                runnableList.add(new DataUtilThread(subList, functionCode));
             }
 
         this.threadArrayList = ThreadGenerator.generateThreads(nOfThreads, runnableList);
     }
 
-    public void runThreads(){
-
+    public void runThreads() {
+        setThreadArrayList(threadArrayList);
+        // starting threads
         for(int i = 0; i < nOfThreads; i++){
             Thread currentThread = threadArrayList.get(i);
             currentThread.start();
         }
+        // waiting for threads to finish
+        for(int i = 0; i < nOfThreads; i++){
+            try {
+                threadArrayList.get(i).join();
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        for(int i = 0; i < nOfThreads; i++) {
+            System.out.println(runnableList.get(i).getResult());
+        }
+        
     }
 }
